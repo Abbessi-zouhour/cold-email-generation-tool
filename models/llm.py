9f8897a91f63f1ece39bb3accfabe914813715fd
Zouhour Abbassi <abbassizouhour2001@gmail.com>
2026-06-16 02:33:12 +0200

    Add Streamlit Cloud secrets support

--- models/llm.py
+++ models/llm.py
@@ -1,12 +1,19 @@
 from dotenv import load_dotenv
 import os
+import streamlit as st
 from langchain_groq import ChatGroq
 
 load_dotenv()
 
 def get_llm():
+
+    api_key = os.getenv("GROQ_API_KEY")
+
+    if not api_key:
+        api_key = st.secrets["GROQ_API_KEY"]
+
     return ChatGroq(
         model="llama-3.3-70b-versatile",
-        api_key=os.getenv("GROQ_API_KEY"),
+        api_key=api_key,
         temperature=0.7
     )
